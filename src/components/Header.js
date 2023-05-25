04201c42d9ce027572398f7f9df7475fb6754809
Mjennowww <114575655+Mjennowww@users.noreply.github.com>
2023-05-25 18:54:15 +0200

    next launch time inladen

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,3 @@
-import React, {useContext} from 'react';
 import logo from '../assets/logo.png';
 import { Link } from 'react-router-dom';
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function Home() {
+    const [nextLaunch, setNextLaunch] = useState(null);
+
+    useEffect(() => {
+        async function fetchNextLaunch() {
+            try {
+                const response = await axios.get('https://api.spacexdata.com/v4/launches/next');
+                console.log(response.data);
+                setNextLaunch(response.data);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+
+        fetchNextLaunch();
+    }, []);
+
+    const formatNextLaunchTime = () => {
+        if (nextLaunch && nextLaunch.date_local) {
+            const launchTime = new Date(nextLaunch.date_local);
+            return launchTime.toLocaleString('nl-NL', {
+                timeZone: 'Europe/Amsterdam',
+            });
+        }
+        return '';
+    };
+
     return (
         <>
-
             <section id="home" className="outer-content-container">
-                <div className="inner-content-container " >
-           <h1 style={{ textAlign: 'center' }}> EXPLORE OUR<br/>NEW HOME</h1>
+                <div className="inner-content-container">
+                    <h1 style={{ textAlign: 'center' }}>EXPLORE OUR<br />NEW HOME</h1>
 
-
+                    <article className="headquarters">
+                        Next Launch:&nbsp;<div>{formatNextLaunchTime()}</div>
+                    </article>
                 </div>
-
             </section>
-
         </>
     );
 }
